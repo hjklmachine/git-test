cac159fa9e4a996aa488150176160c2d088a1c0e
hjklmachine <hjklmachine@outlook.com>
2023-02-13 09:37:16 +0000

    add database handler

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include "utils/mathclass.cpp"
+#include "utils/database_handler.cpp"
 
 using namespace std;
 int main(){
   Mathclass mc(2,4);
   cout <<  mc.sum() << endl;
+
+  Database_handler dh;
+  dh.connect();
 
 }
